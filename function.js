3f1557f7babe7df1c49c20b4b6bc253f98f1ecce
Easa Yeasfi Bin Islam <oritrobinislam@gmail.com>
2024-07-10 15:20:48 +0600

    func done

---
+++ function.js
@@ -0,0 +1,11 @@
+//  Declaring and caling function;
+function pc(){
+    console.log('Press the switch');
+    console.log('Enter pass codes');
+    console.log('adn then');
+    console.log('Turn on your PC');
+
+}
+pc();
+
+// Don't think I'm dumb moron. Just creating more repo. 
